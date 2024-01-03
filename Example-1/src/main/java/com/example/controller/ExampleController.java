e2fae34383f4c07d9345fc224e2114304bdccb79
gopalakrishnan.g04 <gopalakrishnan.g04@CHNSHL564555L.ad.infosys.com>
2024-01-03 12:34:03 +0530

    changes

--- Example-1/src/main/java/com/example/controller/ExampleController.java
+++ Example-1/src/main/java/com/example/controller/ExampleController.java
@@ -1,15 +1,25 @@
 package com.example.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.service.ExampleService;
 
 @RestController
 public class ExampleController {
 
+	@Autowired ExampleService exp;
 	
 	 @GetMapping("/hello")
-	  public String getHello()
+	  public static String getHello()
 	   {
 	    return "Hello";
+	   }
+	  @GetMapping("/cal")
+	  public double cal() {
+		  
+		  return exp.getCal('*');
+		  
 	  }
 }
---
+++ Example-1/src/main/java/com/example/service/ExampleService.java
@@ -0,0 +1,50 @@
+package com.example.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ExampleService {
+
+	
+	 char op;
+
+	public double getCal(char op) {
+	    double num1=2;
+	    double num2=3;
+	 this.op=op;
+	 double o = 0;
+
+	 switch (op) {
+	 // case to add two numbers
+	 case '+':
+	     o = num1 + num2;
+	     break;
+
+	 // case to subtract two numbers
+	 case '-':
+	     o = num1 - num2;
+	     break;
+
+	 // case to multiply two numbers
+	 case '*':
+	     o = num1 * num2;
+	     break;
+
+	 // case to divide two numbers
+	 case '/':
+	     o = num1 / num2;
+	     break;
+
+	 default:
+	     System.out.println("You enter wrong input");
+	 }
+
+	 System.out.println("The final result:"+ o);
+	 System.out.println();
+
+	 // print the final result
+	return o;
+
+	}
+
+}
